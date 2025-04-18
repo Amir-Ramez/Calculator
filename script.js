b86b7030acb2735c29160435a01476e3e7a92971
Amir <109381498+Amir-Ramez@users.noreply.github.com>
2025-04-18 14:01:02 +0200

    Implement equal functionality for evaluating expressions
    - Handled both operator chaining and equal button usage
    - Ensured operands are parsed correctly and display updates after calculation
    - Reset secondOperand and operator after evaluation to support chaining

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let firstOperand = '',
     secondOperand = '',
     operator = '';
+let result;
 
 const operands = document.querySelectorAll('.numericalButton');
 const operators = document.querySelectorAll('.operator');
 let currentInput = document.querySelector('.display .current');
 let inputHistory = document.querySelector('.display .history');
+let equalButton = document.querySelector('.equalButton');
 
 operands.forEach((numericalButton) => {
     numericalButton.addEventListener('click', (e) => {
         if (operator === '') {
+            if (result) firstOperand = '';
+
             firstOperand = firstOperand + e.target.textContent;
             currentInput.textContent = firstOperand;
         } else if (firstOperand && operator !== '') {
@@ -24,6 +28,35 @@ operators.forEach((operatorButton) => {
         if (firstOperand && !secondOperand) {
             operator = e.target.textContent;
             inputHistory.textContent = firstOperand + ' ' + operator;
+        } else if (firstOperand && secondOperand) {
+            evaluate();
+            operator = e.target.textContent;
+            inputHistory.textContent = firstOperand + ' ' + operator;
         }
     });
+});
+
+function evaluate() {
+    result = equal(
+        parseFloat(firstOperand),
+        parseFloat(secondOperand),
+        operator
+    );
+
+    inputHistory.innerHTML = '';
+    currentInput.textContent = `${result}`;
+    firstOperand = String(result);
+    secondOperand = '';
+    operator = '';
+}
+
+function equal(firstOperand, secondOperand, operator) {
+    if (operator === '+') return firstOperand + secondOperand;
+    else if (operator === '-') return firstOperand - secondOperand;
+    else if (operator === '*') return firstOperand * secondOperand;
+    else if (operator === '/') return firstOperand / secondOperand;
+}
+
+equalButton.addEventListener('click', () => {
+    evaluate();
 });
